b7b08c013a45e7a1e2c9b3cf98127bb58dca6ada
Yongfei Lu <yflecon@gmail.com>
2020-09-09 14:10:20 -0500

    Initial Commit

---
+++ chatF/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatF
+//
+//  Created by 卢勇霏 on 9/9/20.
+//  Copyright © 2020 Yongfei Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
